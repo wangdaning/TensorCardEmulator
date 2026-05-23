7c0df394eb4e329f10b9cbaf63cf5ad5b00500b1
wangdaning <mynamewasgrimm@gmail.com>
2026-05-23 17:20:32 +0800

    Shifting things around in module/Update apk signing

--- MagiskModule/customize.sh
+++ MagiskModule/customize.sh
@@ -2,11 +2,8 @@
 
 SKIPMOUNT=false
 
-# 1. Clean up to avoid conflicts
 rm -rf "$MODPATH/system/priv-app"
 rm -rf "$MODPATH/system/vendor"
-
-mkdir -p "$MODPATH/staging"
 
 if [ -f "$MODPATH/common/TensorCardEmulator.apk" ]; then
     cp "$MODPATH/common/TensorCardEmulator.apk" "$MODPATH/staging/TensorCardEmulator.apk"
@@ -15,13 +12,11 @@ else
     abort
 fi
 
-# Install apk
-ui_print "- Sideloading apk as user..."
-pm install -r -g --user 0 "$MODPATH/staging/TensorCardEmulator.apk"
 
-ui_print "- Flash installation complete. Processing boot scripts..."
+ui_print "- APK staged"
 
+set_perm "$MODPATH/service.sh" root root 0755
 set_perm "$MODPATH/uninstall.sh" root root 0755
 
-ui_print "- Extraction and configuration successful!"
+ui_print "- Flash complete. App will install on next boot."
 
