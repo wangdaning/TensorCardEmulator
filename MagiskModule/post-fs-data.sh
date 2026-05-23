db1dcd9ad17aa586bc35fffbc38cfc3f6e7310d1
wangdaning <mynamewasgrimm@gmail.com>
2026-05-24 05:05:44 +0800

    fix freezing at lockscreen after flashing and reboot

--- MagiskModule/post-fs-data.sh
+++ MagiskModule/post-fs-data.sh
@@ -1,9 +1,9 @@
 #!/system/bin/sh
 
 MODDIR="/data/adb/modules/TensorCardEmulator"
-
-# Prepare NFC backup/Spoofing directories
-echo "- Backing up and spoofing STMicroelectronics NFC configurations..."
+PKG="ru.extreames.tensorcardemulator"
+PKGXML="/data/system/packages.xml"
+PKGXML_BAK="/data/system/packages.xml.tce_bak"
 
 mkdir -p /data/adb/TensorCardEmulator
 mkdir -p "$MODDIR/system/vendor/etc"
@@ -25,3 +25,48 @@ fi
 cp -f /vendor/etc/libnfc-hal-st.conf "$MODDIR/system/vendor/etc/" 2>/dev/null
 cp -f /vendor/etc/libnfc-hal-st-proto1.conf "$MODDIR/system/vendor/etc/" 2>/dev/null
 
+# Only patch packages.xml if the app is already installed
+if [ ! -f "$PKGXML" ]; then
+    exit 0
+fi
+
+if ! grep -q "\"${PKG}\"" "$PKGXML"; then
+    # App not installed yet, nothing to patch
+    exit 0
+fi
+
+# Patch FLAG_SYSTEM
+CURRENT_FLAGS=$(grep -A2 "\"${PKG}\"" "$PKGXML" | grep -o 'flags="[0-9]*"' | head -1 | grep -o '[0-9]*')
+if [ -n "$CURRENT_FLAGS" ]; then
+    NEW_FLAGS=$(( CURRENT_FLAGS | 1 ))
+    if [ "$NEW_FLAGS" != "$CURRENT_FLAGS" ]; then
+        cp "$PKGXML" "$PKGXML_BAK"
+        awk -v pkg="$PKG" -v old="$CURRENT_FLAGS" -v new="$NEW_FLAGS" '
+            /package name="/ && $0 ~ pkg { found=1 }
+            found && /flags="/ {
+                sub("flags=\"" old "\"", "flags=\"" new "\"")
+                found=0
+            }
+            { print }
+        ' "$PKGXML_BAK" > "$PKGXML"
+    fi
+fi
+
+# Inject NFC permissions
+if ! grep -A20 "\"${PKG}\"" "$PKGXML" | grep -q "NFC_PREFERRED_PAYMENT_SERVICE"; then
+    cp "$PKGXML" "${PKGXML}.tce_pre_perms"
+    awk -v pkg="$PKG" '
+        /package name="/ && $0 ~ pkg { in_pkg=1 }
+        in_pkg && /<perms>/ {
+            print
+            print "            <item name=\"android.permission.NFC\" granted=\"true\" flags=\"0\" />"
+            print "            <item name=\"android.permission.BIND_NFC_SERVICE\" granted=\"true\" flags=\"0\" />"
+            print "            <item name=\"android.permission.NFC_PREFERRED_PAYMENT_SERVICE\" granted=\"true\" flags=\"0\" />"
+            print "            <item name=\"android.permission.NFC_PREFERRED_PAYMENT_INFO\" granted=\"true\" flags=\"0\" />"
+            print "            <item name=\"android.permission.CHANGE_NFC_STATE\" granted=\"true\" flags=\"0\" />"
+            next
+        }
+        in_pkg && /<\/package>/ { in_pkg=0 }
+        { print }
+    ' "${PKGXML}.tce_pre_perms" > "$PKGXML"
+fi
