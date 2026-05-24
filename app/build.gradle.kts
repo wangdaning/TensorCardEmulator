c7e50bf21706f025274f683532a0d9534640ddb6
wangdaning <mynamewasgrimm@gmail.com>
2026-05-24 10:31:21 +0800

    remove signing in gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,6 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-			signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
