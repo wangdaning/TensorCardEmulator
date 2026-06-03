9e677148079b287536f825658baa261cea9e3363
wangdaning <mynamewasgrimm@gmail.com>
2026-06-03 13:51:26 +0800

    change dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
 }
 
 dependencies {
-    compileOnly(libs.libxposed)
+    implementation(libs.libxposed)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation("androidx.recyclerview:recyclerview:1.3.2")
