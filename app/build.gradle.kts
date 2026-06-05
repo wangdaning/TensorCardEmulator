7f2ee04fa3a589f2da8f8c5b9c946407b6b3a6c8
wangdaning <mynamewasgrimm@gmail.com>
2026-06-05 09:28:01 +0800

    Update again

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "ru.extreames.tensorcardemulator"
-    compileSdk = 36
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "ru.extreames.tensorcardemulator"
         minSdk = 34
-        targetSdk = 36
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
